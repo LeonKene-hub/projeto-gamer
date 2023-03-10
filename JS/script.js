849f000865ffd96756b528d56ed5ab25f2901aff
Pedro H. Alves <leonkene18@gmail.com>
2023-03-09 22:21:48 -0300

    Menu pronto

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,7 @@
 function MostrarMenu() {
     document.getElementById("menu-mobile1").classList.toggle("MostrarMenu");
 }
+
+function MostrarMenu2() {
+    document.getElementById("menu-mobile2").classList.toggle("MostrarMenu2");
+}
