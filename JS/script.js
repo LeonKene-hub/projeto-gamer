87660efffd3d3cb6048d0c7ffce41b20f56f8098
Pedro H. Alves <leonkene18@gmail.com>
2023-03-21 10:52:41 -0300

    AAAAAAAAAA JS

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,14 @@ function MostrarMenu() {
 function MostrarMenu2() {
     document.getElementById("menu-mobile2").classList.toggle("MostrarMenu2");
 }
+
+function Logar() {
+    let email = document.getElementById("email-login")
+    let senha = document.getElementById("senha-login")
+    
+    if (email.value.length == 0, senha.value.length ==0){
+        window.alert("Por favor, digite seu email e senha")
+    } else {
+        window.location.href = "../jogadores.html"
+    }
+}
