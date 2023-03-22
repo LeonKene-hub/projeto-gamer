cd8d2cd0e476f6f57fce5236ddb9015c2ca95759
Pedro H. Alves <leonkene18@gmail.com>
2023-03-22 08:05:12 -0300

    Foguete nao tem re

--- JS/script.js
+++ JS/script.js
@@ -6,13 +6,3 @@ function MostrarMenu2() {
     document.getElementById("menu-mobile2").classList.toggle("MostrarMenu2");
 }
 
-function Logar() {
-    let email = document.getElementById("email-login")
-    let senha = document.getElementById("senha-login")
-    
-    if (email.value.length == 0, senha.value.length ==0){
-        window.alert("Por favor, digite seu email e senha")
-    } else {
-        window.location.href = "../jogadores.html"
-    }
-}
